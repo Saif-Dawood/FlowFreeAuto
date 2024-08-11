69194116fb751840f4fd7e738148770534b42ec1
Saif-Dawood <saifdawoodcs@gmail.com>
2024-08-11 22:04:18 +0300

    [UPDT] Program: Hello World the program

--- FlowFreeGame/Program.cs
+++ FlowFreeGame/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+// Main Program Class
+class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("HelloWorld!");
+    }
+}
