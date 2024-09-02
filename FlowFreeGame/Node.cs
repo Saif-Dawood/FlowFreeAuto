a0ff6521e93263e9ff69f346aaed3e207005c192
Saif-Dawood <saifdawoodcs@gmail.com>
2024-09-02 15:28:45 +0300

    [ADD] Node Class

---
+++ FlowFreeGame/Node.cs
@@ -0,0 +1,26 @@
+
+
+
+
+// The Node that is connected by the pipe line in the main game
+class Node
+{
+    // x (column in board)
+    // y (row in board)
+    private int x, y;
+    public Node(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+}
