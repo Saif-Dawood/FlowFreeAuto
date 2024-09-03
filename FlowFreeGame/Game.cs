2a64f544f4bc3508c23a191961db5204cbfe2044
Saif-Dawood <saifdawoodcs@gmail.com>
2024-09-03 14:55:29 +0300

    [UPDT] Game: ADD printBoard function

--- FlowFreeGame/Game.cs
+++ FlowFreeGame/Game.cs
@@ -12,7 +12,10 @@ class Game
     public Game(int height, int width, int numPipes, int seed=0)
     {
         // Create a board with height and width
-        board = new string[height,width];
+        board = new string[width,height];
+        for (int j = 0; j < height; ++j)
+            for (int i = 0; i < width; ++i)
+                board[i,j] = "";
 
         // Check if numPipes is bigger than what could be done
         // TODO
@@ -28,6 +31,7 @@ class Game
             rand = new Random(seed);
         // Colors
         var colors = Enum.GetValues(typeof(Color)).Cast<Color>().GetEnumerator();
+        colors.MoveNext();
         for (int i = 0; i < numPipes; ++i)
         {
             int x1, y1, x2, y2;
@@ -50,10 +54,52 @@ class Game
         else if (x1 == x2 || y1 == y2)
             return Globals.StartEqualEnd;
         
-        board[x1,y1] = "start" + color.ToString();
-        board[x2,y2] = "end" + color.ToString();
+        board[x1,y1] = "s" + color.ToString().Substring(0,2);
+        board[x2,y2] = "e" + color.ToString().Substring(0,2);
 
         return 0;
+    }
+
+    // A function to view curent board
+    public void printBoard()
+    {
+        int maxwidth = 0;
+        for (int j = 0; j < this.board.GetLength(1); ++j)
+        {
+            int curwidth = 1;
+            for (int i = 0; i < this.board.GetLength(0); ++i)
+            {
+                if (this.board[i,j] == "")curwidth += 3;
+                else curwidth += this.board[i,j].Length;
+                curwidth += 1;
+            }
+            maxwidth = Math.Max(maxwidth, curwidth);
+        }
+
+        // Draw upper border
+        for (int i = 0; i < maxwidth; ++i)
+            Console.Write("_");
+        Console.Write("\n");
+
+        // Draw the table
+        for (int j = 0; j < this.board.GetLength(1); ++j)
+        {
+            Console.Write("|");
+            for (int i = 0; i < this.board.GetLength(0); ++i)
+            {
+                if (this.board[i,j] == "")
+                    Console.Write("   ");
+                else
+                    Console.Write(this.board[i,j]);
+                Console.Write("|");
+            }
+            Console.Write("\n");
+        }
+
+        // Draw lower border
+        for (int i = 0; i < maxwidth; ++i)
+            Console.Write("_");
+        Console.Write("\n");
     }
 
 }
