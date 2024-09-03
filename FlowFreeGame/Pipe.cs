4599e47ddb4757a5288aa7af08018ee0f39536c9
Saif-Dawood <saifdawoodcs@gmail.com>
2024-09-03 10:14:54 +0300

    [UPDT] Pipe class: ADD enum colors

--- FlowFreeGame/Pipe.cs
+++ FlowFreeGame/Pipe.cs
@@ -1,16 +1,39 @@
 
+namespace FlowFreeGame;
+
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Orange,
+    Cyan,
+    Magenta,
+    Brown,
+    Purple,
+    White,
+    Gray,
+    Lime,
+    Beige,
+    Navy,
+    Teal,
+    Pink,
+};
 
 
 // The Pipes in the main game
 class Pipe
 {
     Node start, end;
+    Color color;
     bool connected;
 
-    public Pipe(int x1, int y1, int x2, int y2)
+    public Pipe(int x1, int y1, int x2, int y2, Color color)
     {
         this.start = new Node(x1, y1);
         this.end = new Node(x2, y2);
+        this.color = color;
         this.connected = false;
     }
 }
