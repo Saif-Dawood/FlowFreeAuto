318dfca821bc0b1b1b9837b476e8d1735068c057
Saif-Dawood <saifdawoodcs@gmail.com>
2024-09-07 19:29:54 +0300

    [UPDT] Pipe: ADD Llist path

--- FlowFreeGame/Pipe.cs
+++ FlowFreeGame/Pipe.cs
@@ -25,14 +25,19 @@ enum Color {
 // The Pipes in the main game
 class Pipe
 {
+    LinkedList<Node> path;
     Node start, end;
     Color color;
     bool connected;
 
     public Pipe(int x1, int y1, int x2, int y2, Color color)
     {
+        // Current path, either starts with this.start or this.end, and keeps going till the other node
+        this.path = new LinkedList<Node>();
+
         this.start = new Node(x1, y1);
         this.end = new Node(x2, y2);
+
         this.color = color;
         this.connected = false;
     }
