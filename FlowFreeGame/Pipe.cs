7a5aa51491d9107485ceb62e9ecddbaddfd1a510
Saif-Dawood <saifdawoodcs@gmail.com>
2024-09-08 19:29:53 +0300

    [UPDT] Pipe: REPLACE connected property with isConnected method

--- FlowFreeGame/Pipe.cs
+++ FlowFreeGame/Pipe.cs
@@ -25,10 +25,9 @@ enum Color {
 // The Pipes in the main game
 class Pipe
 {
-    LinkedList<Node> path;
-    Node start, end;
-    Color color;
-    bool connected;
+    private LinkedList<Node> path;
+    private Node start, end;
+    private Color color;
 
     public Pipe(int x1, int y1, int x2, int y2, Color color)
     {
@@ -39,6 +38,11 @@ class Pipe
         this.end = new Node(x2, y2);
 
         this.color = color;
-        this.connected = false;
+    }
+
+    public bool isConnected()
+    {
+        return ((this.path.First<Node>() == this.start && this.path.Last<Node>() == this.end) ||
+            (this.path.Last<Node>() == this.start && this.path.First<Node>() == this.end));
     }
 }
